868cdb44f934afef3bd442082b4653050050d7a0
Neelesh Arunkumar <neelesharunkumar@gmail.com>
2021-01-21 16:12:16 +0530

    Update API_Call.py

--- API_Call.py
+++ API_Call.py
@@ -5,6 +5,18 @@ for i in range(1):
     myobj = {"true": i}
 
     x = requests.post(url, json = myobj)
+    
+    @exception
+def createSubElementText(Parent, tagName, xmlAttributeDict, tagText=None):
+    
+    tag = et.SubElement(Parent, tagName, xmlAttributeDict)
+    if tagText is None:
+        tag.text = ''
+    else:
+        tag.text = str(tagText).replace(chr(160), ' ').strip()
+    #print(tag.text)
+    logging.info("{} tag is generated as {} or {}".format(str(tagName), tagText, str(xmlAttributeDict.values())))
+    return tag
 
     #print(x.text)
 
