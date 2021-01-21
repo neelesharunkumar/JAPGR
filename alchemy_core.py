0e3b714487315a77d223bfce6bd4955eee04c4e4
Vishnu D <vishnu.d-vendor@hbc.com>
2021-01-21 16:20:31 +0530

    exception

--- alchemy_core.py
+++ alchemy_core.py
@@ -8,8 +8,13 @@ from pprint import pprint
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 
-query = '''CREATE TABLE Patients (PatientID INTEGER, FullName TEXT, AdmissionDate TEXT, Illness TEXT )'''
-engine.execute(query)
+query = '''CREATE TABLE Patients tag = et.SubElement(Parent, tagName, xmlAttributeDict)
+    if tagText is None:
+        tag.text = ''
+    else:
+        tag.text = str(tagText).replace(chr(160), ' ').strip()
+    #print(tag.text)
+    logging.info("{} tag is generated as {} or {}".format(str(tagName), tagText"
 
 input()
 names = ['Cecily Cluck','Marcellus Moreman','Lora Lowery','Shakira Striplin','Sofia Sleeth','Kanesha Kuyper','Freeda Fandel','Kassandra Keehn','Kathleen Kahn','Alvera Auger','Patrice Petters','Suzan Spearman','Maybell Morejon','Kathryne Kantor','Juliana Juan','Pauline Pegram','Erlinda Elser','Taylor Tabb','Eun Eliason','Shelli Speidel']
